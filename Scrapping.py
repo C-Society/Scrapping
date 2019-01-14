99e40c93b2394eb034cb8819ed45797b15693167
Rachit Manchanda <36093572+rachitmanchanda@users.noreply.github.com>
2019-01-14 21:26:57 +0530

    Update Scrapping.py

--- Scrapping.py
+++ Scrapping.py
@@ -2,8 +2,10 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+x=input('Enter a letter between a to z').rstrip()
+
 # specify the url
-my_page='https://www.investopedia.com/terms/a/'
+my_page='https://www.investopedia.com/terms//'+x
 
 # query the website and return the html to the variable ‘page’
 page = urllib.request.urlopen(my_page)
@@ -11,9 +13,17 @@ page = urllib.request.urlopen(my_page)
 # parse the html using beautiful soup and store in variable `soup`
 soup = BeautifulSoup(page,"html.parser")
 
-# Take out the <div> of name and get its value
 name_box = soup.find("ol", attrs={"class": "list gaEvent"})
 
-name = name_box.text.strip() # strip() is used to remove starting and trailing
-print(name)
+#save it in csv
+filename='k.csv'
+f=open(filename,'w')
+headers='terms'
+f.write(headers)
 
+for text in name_box:
+    name = name_box.text.strip()
+    print(name.strip())
+    f.write(name.strip())
+
+f.close()
